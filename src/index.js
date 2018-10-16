9070f6991cfefc8701cba8a0a3e690a34715e638
Fr0nny <fronnythedev@gmail.com>
2018-10-16 14:58:08 +0200

    deleted all firebase stuff

--- src/index.js
+++ src/index.js
@@ -9,17 +9,8 @@ import { Router, Route, Switch } from "react-router";
 import indexRoutes from "routes/index.jsx";
 
 import "assets/scss/material-kit-react.css";
-import firebase from 'firebase/app';
 
-var config = {
-  apiKey: "AIzaSyDQjslYmKz7nljvqRdt7u7jfb7vdgjzHzg",
-    authDomain: "triggered-homepage.firebaseapp.com",
-    databaseURL: "https://triggered-homepage.firebaseio.com",
-    projectId: "triggered-homepage",
-    storageBucket: "triggered-homepage.appspot.com",
-    messagingSenderId: "994044325551"
-};
-firebase.initializeApp(config);
+
 
 
 var hist = createBrowserHistory();
